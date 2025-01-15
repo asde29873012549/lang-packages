d9f85670390104dd0514021bd59fcf1a85418056
noah <noah.hong@langlive.com>
2025-01-15 16:07:06 +0800

    docs: ✏️ add comments for styled-px2rem regex

--- packages/styled-px2rem-loader/lib/constants.js
+++ packages/styled-px2rem-loader/lib/constants.js
@@ -3,10 +3,13 @@ const CSS_CLOSING_PLACEHOLDER = "} /* px2rem closing placeholder */";
 const CSS_PROPERTY_PLACEHOLDER =
   "/* start: px2rem property placeholder */ padding: /* end: px2rem property placeholder */";
 
+// paire regular css property rg. padding: 10px;
 const CSS_PAIR_REGEX = /[\s\w-]+:\s*(-?\d*\.?\d+)px/;
+// px regular expression eg. 12px
 const PX_REGEX = /(?<![\w-])(-?\d*\.?\d+\s*px)\b/;
+// px regular expression global
 const PX_REGEX_GLOBAL = /(?<![\w-])(-?\d*\.?\d+\s*px)\b/g;
-
+// disable comment regular expression eg. /* styled-px2rem-disable */
 const DISABLE_COMMENT_REGEX = /\s*styled-px2rem-disable\s*/;
 
 module.exports = {
